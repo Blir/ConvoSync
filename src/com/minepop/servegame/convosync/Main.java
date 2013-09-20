c3fbb947c24bd9791856abbc773457b73c7ede7a
Travis Bruce <random11714@yahoo.com>
2013-09-20 16:55:13 -0500

    v1.0.3 dev 4.7 Apparently version numbers are supposed to actually change.

--- src/com/minepop/servegame/convosync/Main.java
+++ src/com/minepop/servegame/convosync/Main.java
@@ -12,7 +12,7 @@ import javax.swing.UnsupportedLookAndFeelException;
  */
 public class Main {
 
-    public static final String VERSION = "1.0.3 Dev 4.6";
+    public static final String VERSION = "1.0.3 Dev 4.7";
 
     public static enum Action {
 
