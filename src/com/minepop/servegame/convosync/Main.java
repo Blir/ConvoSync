0dd58780ec954382bddfef9dc67fc0f6fd61e921
Travis Bruce <random11714@yahoo.com>
2013-09-28 20:00:33 -0500

    1.0.4-dev1.9+debug

--- src/com/minepop/servegame/convosync/Main.java
+++ src/com/minepop/servegame/convosync/Main.java
@@ -12,7 +12,7 @@ import javax.swing.UnsupportedLookAndFeelException;
  */
 public class Main {
 
-    public static final String VERSION = "1.0.4-dev1.9";
+    public static final String VERSION = "1.0.4-dev1.9+debug";
 
     public static enum Action {
 
--- src/com/minepop/servegame/convosync/plugin/EssentialsListener.java
+++ src/com/minepop/servegame/convosync/plugin/EssentialsListener.java
@@ -2,6 +2,7 @@ package com.minepop.servegame.convosync.plugin;
 
 import com.earth2me.essentials.Essentials;
 import com.minepop.servegame.convosync.net.PlayerVanishMessage;
+import java.util.logging.Level;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -27,6 +28,8 @@ public class EssentialsListener implements Listener {
 
     @EventHandler
     public void onPlayerCommandPreprocessEvent(PlayerCommandPreprocessEvent evt) {
+        plugin.getLogger().log(Level.INFO, "isEss: {0} Canceled: {1} Vanished: {2}",
+                new Object[]{plugin.isEss, evt.isCancelled(), ess.getUser(evt.getPlayer()).isVanished()});
         if (plugin.isEss && !evt.isCancelled() && evt.getMessage().equalsIgnoreCase("vanish")) {
             plugin.out(new PlayerVanishMessage(evt.getPlayer().getName(), !ess.getUser(evt.getPlayer()).isVanished()), false);
         }
